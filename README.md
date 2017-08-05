315f00d94aa92652e73995c5e3d2cb85e2febe1d
Wes Van Duine <wesvanduine@gmail.com>
2017-08-04 19:56:59 -0700

    burger model

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# Burger
-In this assignment, you'll create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
-=======
 # sequelizedBurger
 sequelizedBurger
->>>>>>> a971df17f575ed243506f0806eed492f596c4380
+
--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,16 @@
-var orm = require('../config/orm.js');
+module.exports = function(sequelize, DataTypes) {
 
-var burger = {
-    all: function(cb) {
-        orm.all('burgers', function(res) {
-            cb(res);
-        })
-    },
-
-    update: function(id, cb) {
-        orm.update('burgers', id, cb);
-    },
-
-    create: function(name, cb) {
-        orm.create('burgers', name, cb);
-    }
-}
-
-module.exports = burger;
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            len: [1]
+        }
+    });
